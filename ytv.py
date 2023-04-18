b9a02ba226b3df60642d93cc994cc454d4f76fa1
rileymurphy93 <31573433+rileymurphy93@users.noreply.github.com>
2023-04-18 01:42:31 +0000

    Update ytv.py

--- ytv.py
+++ ytv.py
@@ -3,13 +3,15 @@ import re
 import urllib.parse
 
 from urllib.parse import quote
+
 search_keyword = input('Search For:')
 
 print('Top Result For:' + search_keyword)
 
-
-
 html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + quote(search_keyword))
+
 video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
 
-print("https://www.youtube.com/watch?v=" + video_ids[0])
+sauce = ("https://www.youtube.com/watch?v=" + video_ids[0])
+
+print(sauce)
